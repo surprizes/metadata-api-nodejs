4f58a97479b34ffc6b7e9f148cbb3852e3a6f041
Lloyd Faulk <>
2020-11-13 12:11:58 -0800

    simplified the database structure and fixed index.js calls to birthday zodiac function

--- index.js
+++ index.js
@@ -1,58 +1,55 @@
-const express = require('express')
-const path = require('path')
-const moment = require('moment')
-const { HOST } = require('./src/constants')
-const db = require('./src/database')
+/** @format */
 
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const moment = require("moment");
+const { HOST } = require("./src/constants");
+const db = require("./src/database");
+
+const PORT = process.env.PORT || 5001;
 
 const app = express()
-  .set('port', PORT)
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .set("port", PORT)
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs");
 
 // Static public files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res) {
-  res.send('Get ready for Surprizes!');
-})
+app.get("/", function (req, res) {
+  res.send("Get ready for Surprizes!");
+});
 
-app.get('/api/token/:token_id', function(req, res) {
-  const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
+app.get("/api/token/:token_id", function (req, res) {
+  const tokenId = parseInt(req.params.token_id).toString();
+  const person = db[tokenId];
+  // const bdayParts = person.birthday.split(' ')
+  // const day = parseInt(bdayParts[1])
+  // const month = parseInt(bdayParts[0])
   const data = {
-    'name': person.name,
-    'description': person.description,
-    'edition': person.edition,
-    'series': person.series,
-    'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      'age': moment().diff(person.birthday, 'years')
-    },
-    'image': person.image
-  }
-  res.send(data)
-})
+    name: person.name,
+    description: person.description,
+    edition: person.edition,
+    series: person.series,
+    artist: person.artist,
+    image: person.image,
+  };
+  res.send(data);
+});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-})
+app.listen(app.get("port"), function () {
+  console.log("Node app is running on port", app.get("port"));
+});
 
 // returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
+// function zodiac(day, month) {
+//   var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
+//   var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
+//   return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
+// }
 
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
+// function monthName(month) {
+//   const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+//   ]
+//   return monthNames[month - 1]
+// }
